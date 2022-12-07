85dd5fedcd2210fd791bf05cf15d3ca48ea435f5
fx <903921845@qq.com>
2022-12-07 22:37:13 +0800

    完成rollup打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,59 @@
 // rollup 配置文件
+import path from "path";
+import json from '@rollup/plugin-json';
+import pluginResolve from '@rollup/plugin-node-resolve';
+import ts from 'rollup-plugin-typescript2';
 
-console.log(process.env.TARGET,'rollup');
+// 根据环境变量中的target属性，获取对应模块的打包信息(package.json)
+
+// 获取当前目录下的packages文件夹
+const packagesDir = path.resolve(__dirname, 'packages')
+
+// 找到packages下的特定(TARGET)模块,用于打包的基准目录
+const packageDir = path.resolve(packagesDir, process.env.TARGET)
+
+// 获取特定模块下的某个文件
+const resolve = (p) => path.resolve(packageDir, p)
+
+const pkg = require(resolve('package.json')) // 引入package.json
+const name = path.basename(packageDir) // 获取文件名
+
+// 打包类型映射表，根据提供的formats，打包相应的格式
+const outputConfig = {
+  'esm-bundler': {
+    file: resolve(`dist/${name}.esm-bundler.js`),
+    format: 'es'
+  },
+  'cjs': {
+    file: resolve(`dist/${name}.cjs.js`),
+    format: 'cjs'
+  },
+  'global': { // 浏览器环境，<script type="module"></script> , 会导出一个全局变量
+    file: resolve(`dist/${name}.global.js`),
+    format: 'iife' // 立即执行函数
+  }
+}
+
+const options = pkg.buildOptions // package.json中的自定义选项
+
+function createConfig (format, output) {
+  console.log('output', output);
+  output.name = options.name
+  output.sourcemap = true
+
+  // 生成roullup打包配置
+  return {
+    input: resolve(`src/index.ts`), // 入口
+    output, // 出口
+    plugins: [ // 插件
+      json(),
+      ts({
+        tsconfig: path.resolve(__dirname, 'tsconfig.json') // 使用的是哪个ts配置文件
+      }),
+      pluginResolve() // 解析第三方模块
+    ]
+  }
+}
+
+// 导出rollup打包配置 
+export default options.formats.map(format => createConfig(format, outputConfig[format]))
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,1 +1,26 @@
 // 只针对某个包(模块)进行打包
+const fs = require('fs')
+const execa = require('execa') // 开启一个子进程，使用rollup进行打包
+
+// 读取packages文件夹,并过滤出文件夹(去掉文件,如README.MD)
+const targets = fs.readdirSync('packages').filter(f => {
+  if (!fs.statSync(`packages/${f}`).isDirectory()) {
+    return false
+  }
+  return true
+})
+
+const target = 'reactivity'
+build()
+
+// 打包方式（打包过程是异步）
+async function build (target) {
+  // rollup -cw --environment TARGET:target 执行命令，并且附带参数
+  // -cw 监听模块，每次修改都会重新打包
+  await execa(
+    'rollup',
+    ['-cw', '--environment', `TARGET:${target}`],
+    { stdio: 'inherit' } // 当子进程打包时的信息共享给父进程
+  )
+}
+
